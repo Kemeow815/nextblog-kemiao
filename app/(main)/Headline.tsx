8434980d92bc83f7cea7e15231a1134c2709e468
Kris Yan <90099234+YanDao0313@users.noreply.github.com>
2023-11-30 10:00:57 +0800

    feat: update `OIer` in Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -10,7 +10,7 @@ import { SocialLink } from '~/components/links/SocialLink'
 function Developer() {
   return (
     <span className="group">
-      可能是 <span className="font-mono">&lt;</span>Dev
+      可能是 <span className="font-mono">&lt;</span>OIer
       <span className="font-mono">/&gt;</span>
       <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
     </span>
