223216c94e0c982f30051c97e880b10fe3cebedc
Kris Yan <90099234+YanDao0313@users.noreply.github.com>
2023-11-30 09:59:59 +0800

    Update nav.ts

--- config/nav.ts
+++ config/nav.ts
@@ -1,7 +1,7 @@
 export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '博客' },
-  { href: '/memos', text: '随记' },
+  // { href: '/memos', text: '随记' },
   { href: '/dg', text: '花园' },
   { href: '/friends', text: '友链' },
   { href: '/guestbook', text: '留言' },
